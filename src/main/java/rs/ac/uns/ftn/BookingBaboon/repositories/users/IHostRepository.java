c299c2ca12b48713eff8acab1a56f291cd432b13
markomitos <markomitosevic9@gmail.com>
2023-11-18 18:42:15 +0100

    feat: added User repositories

---
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/repositories/users/IHostRepository.java
@@ -0,0 +1,9 @@
+package rs.ac.uns.ftn.BookingBaboon.repositories.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.uns.ftn.BookingBaboon.domain.users.Host;
+
+@Repository
+public interface IHostRepository extends JpaRepository<Host, Long> {
+}
