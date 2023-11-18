cd426840fe90b1c6579a9ec4957ae3956083f302
markomitos <markomitosevic9@gmail.com>
2023-11-18 18:13:03 +0100

    feat: added User models

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/users/Guest.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/users/Guest.java
@@ -1,9 +1,21 @@
 package rs.ac.uns.ftn.BookingBaboon.domain.users;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
+import rs.ac.uns.ftn.BookingBaboon.domain.accommodation_handling.Accommodation;
+import rs.ac.uns.ftn.BookingBaboon.domain.notifications.NotificationType;
+
+import java.util.Set;
 
 @Entity
 @Data
+@Table(name = "guests")
 public class Guest extends User {
+
+    private Set<NotificationType> ignoredNotifications;
+
+    @ManyToMany
+    private Set<Accommodation> favorites;
 }
