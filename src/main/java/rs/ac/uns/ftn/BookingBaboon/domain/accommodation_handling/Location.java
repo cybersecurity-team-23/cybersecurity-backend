03269516d1f0c081b1855597e7f46dbc422ac851
markomitos <markomitosevic9@gmail.com>
2023-11-19 13:13:35 +0100

    fix: removed entity annotations from location embeddable

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/accommodation_handling/Location.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/accommodation_handling/Location.java
@@ -3,15 +3,11 @@ package rs.ac.uns.ftn.BookingBaboon.domain.accommodation_handling;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
+import java.lang.annotation.Annotation;
+
 @Embeddable
 @Data
-@Table(name = "locations")
-@TableGenerator(name="location_id_generator", table="primary_keys", pkColumnName="key_pk", pkColumnValue="location", valueColumnName="value_pk")
 public class Location {
-    @Id
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "location_id_generator")
-    private Long id;
     private String country;
     private String city;
     private String address;
