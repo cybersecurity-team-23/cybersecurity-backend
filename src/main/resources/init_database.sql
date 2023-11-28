893773bd7b0bf02ad12a83077a66383032ccfc13
markomitos <markomitosevic9@gmail.com>
2023-11-28 11:33:39 +0100

    fix: script code now properly initialize database

--- src/main/resources/init_database.sql
+++ src/main/resources/init_database.sql
@@ -1,14 +1,7 @@
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'booking_baboon_database') THEN
-    CREATE DATABASE booking_baboon_database;
-END IF;
-END $$;
+CREATE DATABASE booking_baboon_database;
 
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_user WHERE usename = 'booking_baboons') THEN
-    CREATE USER booking_baboons WITH PASSWORD 'm0nk3';
-    GRANT ALL PRIVILEGES ON DATABASE booking_baboon_database TO booking_baboons;
-END IF;
-END $$;
+\c booking_baboon_database;
+
+CREATE USER booking_baboons WITH PASSWORD 'm0nk3';
+
+GRANT ALL PRIVILEGES ON DATABASE booking_baboon_database TO booking_baboons;
