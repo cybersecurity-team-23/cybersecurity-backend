b8f9b5d4fe2b519a199f58db78df23f6a5f6e7c7
markomitos <markomitosevic9@gmail.com>
2023-12-03 17:29:18 +0100

    feature: encoded all passwords in test data

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/config/security/WebSecurityConfiguration.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/config/security/WebSecurityConfiguration.java
@@ -45,7 +45,6 @@ public class WebSecurityConfiguration{
     @Bean
     public PasswordEncoder passwordEncoder() {
         PasswordEncoder encoder = new BCryptPasswordEncoder();// PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        System.out.println(encoder.encode("password123"));
         return encoder;
     }
 
