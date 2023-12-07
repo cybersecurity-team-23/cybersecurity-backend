fab959925227cd38711f8c600e0f9a269adb8e45
maric300 <branko.maric300@gmail.com>
2023-12-08 00:58:04 +0100

    fix: changed inheritance type to JOINED in review and report models

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/reports/GuestReport.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/reports/GuestReport.java
@@ -6,8 +6,6 @@ import rs.ac.uns.ftn.BookingBaboon.domain.users.Guest;
 @Entity
 @Data
 @Table(name = "guest_reports")
-@TableGenerator(name="guest_report_id_generator", table="primary_keys", pkColumnName="key_pk", pkColumnValue="guest_report", valueColumnName="value_pk")
-
 public class GuestReport extends Report {
 
     @ManyToOne
--- src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/reports/HostReport.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/domain/reports/HostReport.java
@@ -6,7 +6,6 @@ import rs.ac.uns.ftn.BookingBaboon.domain.users.Host;
 @Entity
 @Data
 @Table(name = "host_reports")
-@TableGenerator(name="host_report_id_generator", table="primary_keys", pkColumnName="key_pk", pkColumnValue="host_report", valueColumnName="value_pk")
 
 public class HostReport extends Report {
 
