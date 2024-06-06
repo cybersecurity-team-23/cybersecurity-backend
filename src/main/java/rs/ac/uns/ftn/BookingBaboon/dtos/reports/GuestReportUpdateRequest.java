06436e68f6139520b5b3f4afeb129e239963da12
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-06-06 19:11:41 +0200

    feat: reports dtos validation

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/dtos/reports/GuestReportUpdateRequest.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/dtos/reports/GuestReportUpdateRequest.java
@@ -1,5 +1,8 @@
 package rs.ac.uns.ftn.BookingBaboon.dtos.reports;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import rs.ac.uns.ftn.BookingBaboon.domain.reports.ReportStatus;
 import rs.ac.uns.ftn.BookingBaboon.dtos.users.UserReferenceRequest;
@@ -11,9 +14,19 @@ import java.util.Date;
 
 @Data
 public class GuestReportUpdateRequest {
+    @NotNull
     private UserReferenceRequest reportee;
+
+    @NotNull
     private Date createdOn;
+
+    @NotNull
     private ReportStatus status;
+
+    @Size(min = 1, max = 250)
+    @Pattern(regexp = "[a-zA-Z0-9 .,]+")
     private String message;
+
+    @NotNull
     private GuestReferenceRequest reportedGuest;
 }
--- src/main/java/rs/ac/uns/ftn/BookingBaboon/dtos/reports/HostReportUpdateRequest.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/dtos/reports/HostReportUpdateRequest.java
@@ -1,6 +1,11 @@
 package rs.ac.uns.ftn.BookingBaboon.dtos.reports;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.time.DurationMin;
 import rs.ac.uns.ftn.BookingBaboon.domain.reports.ReportStatus;
 import rs.ac.uns.ftn.BookingBaboon.dtos.users.UserReferenceRequest;
 import rs.ac.uns.ftn.BookingBaboon.dtos.users.UserRequest;
@@ -12,10 +17,22 @@ import java.util.Date;
 
 @Data
 public class HostReportUpdateRequest {
+    @Min(0)
     private Long id;
+
+    @NotNull
     private UserReferenceRequest reportee;
+
+    @NotNull
     private Date createdOn;
+
+    @NotNull
     private ReportStatus status;
+
+    @Size(min = 1, max = 250)
+    @Pattern(regexp = "[a-zA-Z0-9 .,]+")
     private String message;
+
+    @NotNull
     private HostReferenceRequest reportedHost;
 }
