8768550c851ddfe2f8268e5abace61c9c6a7d644
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-06-06 19:13:55 +0200

    feat: reservation request dto validation

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/dtos/reservation/ReservationRequest.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/dtos/reservation/ReservationRequest.java
@@ -1,5 +1,7 @@
 package rs.ac.uns.ftn.BookingBaboon.dtos.reservation;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import rs.ac.uns.ftn.BookingBaboon.domain.shared.TimeSlot;
 import rs.ac.uns.ftn.BookingBaboon.dtos.accommodation_handling.accommodation.AccommodationRequest;
@@ -7,9 +9,18 @@ import rs.ac.uns.ftn.BookingBaboon.dtos.users.guests.GuestResponse;
 
 @Data
 public class ReservationRequest {
+    @Min(0)
     private Long id;
+
+    @NotNull
     private AccommodationRequest accommodation;
+
+    @NotNull
     private TimeSlot timeSlot;
+
+    @NotNull
     private GuestResponse guest;
+
+    @Min(1)
     private Float price;
 }
