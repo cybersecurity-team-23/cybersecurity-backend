24d9577423753f8e230ed5d3ee75e1d16acf9d0b
darkokos <darkosvilar02@gmail.com>
2024-06-10 07:14:31 +0200

    feat: started rbac

--- src/main/java/rs/ac/uns/ftn/BookingBaboon/config/security/WebSecurityConfiguration.java
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/config/security/WebSecurityConfiguration.java
@@ -1,10 +1,14 @@
 package rs.ac.uns.ftn.BookingBaboon.config.security;
 
+import org.keycloak.adapters.authorization.integration.jakarta.ServletPolicyEnforcerFilter;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
+import org.keycloak.util.JsonSerialization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,15 +17,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.config.annotation.web.builders.HttpSecurity.*;
-
+import java.io.IOException;
 // Other imports...
 
 
@@ -32,6 +37,39 @@ public class WebSecurityConfiguration{
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    String jwtSetUri;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+
+    private ServletPolicyEnforcerFilter getPolicyEnforcerFilter() {
+        PolicyEnforcerConfig policyEnforcerConfig;
+
+        try {
+            policyEnforcerConfig =
+                    JsonSerialization
+                            .readValue(
+                                    getClass().getResourceAsStream("/policy-enforcer.json"),
+                                    PolicyEnforcerConfig.class
+                            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ServletPolicyEnforcerFilter(httpRequest -> policyEnforcerConfig);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -44,6 +82,8 @@ public class WebSecurityConfiguration{
         http.headers(headers ->
                 headers.xssProtection(xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK))
                 .contentSecurityPolicy(cps -> cps.policyDirectives("script-src 'self'")));
+//        http.oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()));
+//        http.addFilterBefore(getPolicyEnforcerFilter(), BearerTokenAuthenticationFilter.class);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class); // JWT procesiramo pre autentikacije
 
         return http.build();
@@ -67,5 +107,10 @@ public class WebSecurityConfiguration{
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwtSetUri).build();
     }
 }
